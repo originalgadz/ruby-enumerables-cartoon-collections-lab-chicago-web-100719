623c647687dcfec7f1c5e8ef8120b9b3054102b6
Glenn A Doherty <gadz@earthlink.net>
2019-08-25 02:17:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,9 @@
 def roll_call_dwarves (dwarf_names)
-  dwarf_names.each_with_index { |name, index| puts "#{index+1} #{name}"}
+  dwarf_names.each_with_index {|name, index| puts "#{index+1} #{name}"}
 end
 
-def summon_captain_planet (planeteer)
-  # Your code here
+def summon_captain_planet (planeteer_calls)
+  new_calls=planeteer_calls.collect {|call| "#{call.capitalize}!"}
+  new_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
-
-%w(cat dog wombat).each_with_index { |item, index|
-  hash[item] = index
-}
